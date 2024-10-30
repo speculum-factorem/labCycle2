e0e9e6fcc6804c427311e7562aa2934c5bc5b8dc
maxim <f4gbure@yandex.ru>
2024-10-30 09:41:30 +0300

    v1

--- task2.py
+++ task2.py
@@ -1,11 +1,17 @@
 A = float(input("Введите длину отрезка A: "))
 B = float(input("Введите длину отрезка B: "))
 
-total_length = 0
+if A < 0 or B < 0:
 
-while total_length + B <= A:
-    total_length += B
+    print("Длины отрезков не могут быть отрицательными")
 
-remaining_length = A - total_length
+else:
 
-print("Длина незанятой части отрезка A:", remaining_length)
+    total_length = 0
+
+    while total_length + B <= A:
+        total_length += B
+
+    remaining_length = A - total_length
+
+    print("Длина незанятой части отрезка A:", remaining_length)
